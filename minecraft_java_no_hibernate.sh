1bdec1fa85beb49c52c86aaf25143cb1e77ba6f4
Alaister Leung <contact@alaister.net>
2021-12-05 18:07:13 +0800

    Add startup script for Minecraft Java without the Hibernate plugin

--- minecraft_java_no_hibernate.sh
+++ minecraft_java_no_hibernate.sh
@@ -2,5 +2,4 @@
 MEMORY=$1
 JARFILE=$2
 rm -f ./plugins/anet.jar ./plugins/alaister_net_hibernate.jar
-curl --create-dirs -sL -o ./plugins/alaister_net_hibernate.jar https://api.spiget.org/v2/resources/4441/download
 java -Xms128M -Xmx${MEMORY}M -Dterminal.jline=false -Dterminal.ansi=true -jar ${JARFILE}
