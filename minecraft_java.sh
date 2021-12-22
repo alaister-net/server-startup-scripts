be8e38fb1a2bd5eef5bd64183925ef4da6fdd3dc
Alaister Leung <contact@alaister.net>
2021-12-22 14:35:35 +0800

    Auto create eula.txt

--- minecraft_java.sh
+++ minecraft_java.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 MEMORY=$1
 JARFILE=$2
+echo 'eula=true' > ./eula.txt
 rm -f ./plugins/anet.jar ./plugins/alaister_net_hibernate.jar ./plugins/Hibernate/config.yml
 curl --create-dirs -sL -o ./plugins/alaister_net_hibernate.jar https://api.spiget.org/v2/resources/4441/download
 curl --create-dirs -sL -o ./plugins/Hibernate/config.yml https://github.com/alaister-net/server-startup-scripts/raw/master/minecraft_java/config.yml
