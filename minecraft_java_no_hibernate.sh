36a9a71e37e5664645e371623d8f8f8f08aca51a
Alaister Leung <contact@alaister.net>
2021-12-24 12:45:02 +0000

    Fix git and eula.txt for all eggs

--- minecraft_java_no_hibernate.sh
+++ minecraft_java_no_hibernate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 MEMORY=$1
 JARFILE=$2
+echo 'eula=true' > ./eula.txt
 rm -f ./plugins/anet.jar ./plugins/alaister_net_hibernate.jar
 java -Xms128M -Xmx${MEMORY}M -Dterminal.jline=false -Dterminal.ansi=true -jar ${JARFILE}
